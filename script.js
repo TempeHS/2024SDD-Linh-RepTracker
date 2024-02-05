bfd2e2780044ac4c04d8f32c7b2902bd7b0bd26e
linhNguyen120 <linh.nguyen120@education.nsw.gov.au>
2024-02-05 11:36:50 +1100

    linked up the routine button and the routine landing page

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("logbookButton").addEventListener("click", function () {
     window.location.href = "logbook.html";
 });
+
+document.getElementById("routineButton").addEventListener("click", function () {
+    window.location.href = "routine.html";
+});
